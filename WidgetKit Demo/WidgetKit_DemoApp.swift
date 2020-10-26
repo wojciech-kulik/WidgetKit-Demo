00a31fa2a5ab0f82b16177868b604b17d5ab56e4
Wojciech Kulik <info@wojciechkulik.pl>
2020-10-26 17:39:40 +0100

    Initial Commit

---
+++ WidgetKit Demo/WidgetKit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKit_DemoApp.swift
+//  WidgetKit Demo
+//
+//  Created by Wojciech Kulik on 26/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
